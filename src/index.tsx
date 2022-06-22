f4efe745b5e4457081b356266f799e98ffc0b0c5
Chanho Kim <cksgh063@gmail.com>
2022-06-22 21:47:23 +0900

    feat: baseurl 추가 (git page)

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ if (container) {
   root.render(
     <React.StrictMode>
       <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
           <App />
         </BrowserRouter>
       </Provider>
