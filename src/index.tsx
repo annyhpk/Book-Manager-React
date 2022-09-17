16430d115a6f802121231a921fc9c12d39e0041c
Chanho Kim <cksgh063@gmail.com>
2022-09-18 00:19:23 +0900

    update: vite 전환 후 Deprecated process 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ if (container) {
   root.render(
     <React.StrictMode>
       <Provider store={store}>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <BrowserRouter>
           <App />
         </BrowserRouter>
       </Provider>
